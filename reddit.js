4a15c1b432fe092675febda4c91783620ab2f1a7
Valentin Minkov <minkowalentin@gmail.com>
2021-05-22 01:32:12 +0200

    get various post attributes and return object

--- reddit.js
+++ reddit.js
@@ -18,16 +18,52 @@ const self = {
 
   },
 
+  // TO DO: Get nr of results instead of all results on only one page.
+  // Make API Endpoints 
   getResults: async (nr) => {
     let elements = await self.page.$$('#siteTable > div[class*="thing"]')
-    
-    console.log(elements.length);
+    let results = [];
 
     for (let element of elements) {
 
-      let title = await element.$eval(('p[class="title"]'), node => node.innerText.trim());
-      console.log(title);
+      let [title, rank, postTime, authorUrl, authorName, score, comments, promoted] = await element.evaluate((element) => {
+        const titleNode = element.querySelector('p[class="title"]');
+        const rankNode = element.querySelector('span.rank');
+        const postTimeNode = element.querySelector('div.top-matter > p.tagline > time');
+        const authorUrlNode = element.querySelector('p[class="tagline "] > a[class*="author"]');
+        const authorNameNode = element.querySelector('p[class="tagline "] > a[class*="author"]');
+        const scoreNode = element.querySelector('div[class="score likes"]');
+        const commentsNode = element.querySelector('div[class="entry unvoted"] > div[class="top-matter"] > ul > li[class="first"] > a');
+        const promotedNode = element.querySelector('div.entry.unvoted > div.top-matter > ul > li:nth-child(1) > span.promoted-tag > span.promoted-span')
+
+        return [
+          titleNode && titleNode.innerText.trim(),
+          rankNode && rankNode.innerText.trim(),
+          postTimeNode && postTimeNode.getAttribute('title'),
+          authorUrlNode && authorUrlNode.getAttribute('href'),
+          authorNameNode && authorNameNode.innerText.trim(),
+          scoreNode && scoreNode.innerText.trim(),
+          commentsNode && commentsNode.innerText.trim(),
+          promotedNode && promotedNode.innerText.trim()
+        ];
+      });
+
+      if(promoted === null) {
+        results.push({
+          title,
+          rank,
+          postTime,
+          authorName,
+          authorUrl,
+          score,
+          comments
+        })
+      }
+
+      console.log(results);
     }
+
+    return results;
   }
 }
 
